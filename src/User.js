5a2b60e957a88eae9e40e8427314f79e36108b2b
jyotiDeshmukh1988 <83502437+jyotiDeshmukh1988@users.noreply.github.com>
2022-09-18 23:17:34 -0700

    how to use props

---
+++ src/User.js
@@ -0,0 +1,11 @@
+import React from 'react';
+
+const User = ({data}) => {
+    return(
+        <>
+            <h1>User Component my name is {data.name}</h1>
+        </>
+    )
+}
+
+export default User
